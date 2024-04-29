026d20c1adc797ab546aba146178e700b3d3f9aa
cristian <cristian.usuga.ardila@gmail.com>
2024-04-28 21:20:02 -0500

    variables de base de datos productivo

--- src/app/api/libs/database.js
+++ src/app/api/libs/database.js
@@ -2,10 +2,10 @@ import mysql from "serverless-mysql"
 
 export const db=mysql({
     config:{
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'clinica',
+        host: 'sql5.freesqldatabase.com',
+        user: 'sql5702682',
+        password: 'NZkssBNeKz',
+        database: 'sql5702682',
         port: 3306
     }
 })
